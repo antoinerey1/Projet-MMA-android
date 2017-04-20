21b9f43d76b55eca13c2de0e15611acc180a8fec
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-04-20 14:48:16 +0200

    modification dans dbutil

--- KarateManager/app/src/main/java/fr/mma/karatemanager/DbUtil.java
+++ KarateManager/app/src/main/java/fr/mma/karatemanager/DbUtil.java
@@ -16,7 +16,7 @@ public class DbUtil {
     }
 
     private void insertInTableFighting(Fighting fighting) {
-        dbAccess.GetDb().execSQL("INSERT INTO fighting (opponent, points, opponent_points, id_competition) VALUES (?,?,?,?)", new Object[]{fighting.getOpponent(), fighting.getPoints(), fighting.getOpponent_points(), fighting.getId_competition()});
+        dbAccess.GetDb().execSQL("INSERT INTO fighting (opponent, points, opponent_points, id_competition) VALUES (?,?,?,?)", new Object[]{fighting.getOpponent(), fighting.getPoints(), fighting.getOpponent_points(), fighting.getCompetition().getId()});
     }
     private void insertInTableCompetition(Competition competition) {
         dbAccess.GetDb().execSQL("INSERT INTO competition (name, fdate, location, result)) VALUES (?,?,?,?)",new Object[]{competition.getName(), competition.getDate(), competition.getLocation(), competition.getScore()});
