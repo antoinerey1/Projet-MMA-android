c3403fdcb051ab17b84b98241f93757f075b9ae4
lou731 <marie.marnet.pro@gmail.com>
2017-04-20 15:24:16 +0200

    main activity

--- KarateManager/app/src/main/java/fr/mma/karatemanager/MainActivity.java
+++ KarateManager/app/src/main/java/fr/mma/karatemanager/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.mma.karatemanager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
