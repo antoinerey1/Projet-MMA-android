cb7f25b18ef4d5b84e048b88a6fe7bb9b4580359
MichelFG <michelfigue@outlook.fr>
2017-04-20 15:07:42 +0200

    Ajout des class et Layouts

--- KarateManager/app/src/main/java/fr/mma/karatemanager/MainActivity.java
+++ KarateManager/app/src/main/java/fr/mma/karatemanager/MainActivity.java
@@ -1,10 +1,8 @@
 package fr.mma.karatemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,16 +23,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnClickCompetButton(View view)
-    {}
+    { Intent intent = new Intent(MainActivity.this,
+            AddCompetitionActivity.class);
+        startActivity(intent);}
 
     public void OnClickCombatButton(View view)
-    {}
+    { Intent intent = new Intent(MainActivity.this,
+            AddFightActivity.class);
+        startActivity(intent);}
 
     public void OnClickHistoButton(View view)
-    {}
+    {Intent intent = new Intent(MainActivity.this,
+            HistoricalActivity.class);
+        startActivity(intent);}
 
     public void OnClickListCompetButton(View view)
-    {}
+    {Intent intent = new Intent(MainActivity.this,
+            CompetitionListActivity.class);
+        startActivity(intent);}
 
 
 
