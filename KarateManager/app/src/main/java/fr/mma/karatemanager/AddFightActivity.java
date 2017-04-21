e0980afe5e40be55911ca26d1321880780265902
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-04-21 16:24:03 +0200

    .

--- KarateManager/app/src/main/java/fr/mma/karatemanager/AddFightActivity.java
+++ KarateManager/app/src/main/java/fr/mma/karatemanager/AddFightActivity.java
@@ -1,8 +1,13 @@
 package fr.mma.karatemanager;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Mitch on 20/04/2017.
@@ -13,5 +18,33 @@ public class AddFightActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_fight);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
+    public void onClick(View view) {
+
+        EditText compet = (EditText) findViewById(R.id.autocompleteCompet);
+        EditText opponent = (EditText) findViewById(R.id.editOpponent);
+        EditText scoreperso = (EditText) findViewById(R.id.editScorePerso);
+        EditText scoreopp = (EditText) findViewById(R.id.editScoreOpponent);
+
+        switch (view.getId()) {
+            case (R.id.addButton):
+                if (!compet.getText().toString().isEmpty() && !opponent.getText().toString().isEmpty() && !scoreperso.getText().toString().isEmpty() && !scoreopp.getText().toString().isEmpty() ) {
+                    Fighting fighting = new Fighting();
+                    fighting.setId_competition(Integer.parseInt(compet.getText().toString()));
+                    fighting.setOpponent(opponent.getText().toString());
+                    fighting.setPoints(Integer.parseInt(scoreperso.getText().toString()));
+                    fighting.setOpponent_points(Integer.parseInt(scoreopp.getText().toString()));
+
+
+
+                    DbUtil dbUtils = new DbUtil(this);
+                    dbUtils.insertInTableFighting(fighting);
+                    Toast.makeText(this,"Sauvegarde ok!", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(this,"Formulaire incomplet!", Toast.LENGTH_LONG).show();
+                }
+        }
+
     }
 }
