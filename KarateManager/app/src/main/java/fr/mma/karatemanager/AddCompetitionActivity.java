35f51854e4ba3fbf9ce00c2040e5591452acdaa5
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-04-21 15:11:59 +0200

    modification du thème

--- KarateManager/app/src/main/java/fr/mma/karatemanager/AddCompetitionActivity.java
+++ KarateManager/app/src/main/java/fr/mma/karatemanager/AddCompetitionActivity.java
@@ -1,8 +1,16 @@
 package fr.mma.karatemanager;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Mitch on 20/04/2017.
@@ -13,5 +21,30 @@ public class AddCompetitionActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_competition);
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
+    public void onClick(View view) {
+
+        EditText nom = (EditText) findViewById(R.id.editCompet);
+        EditText date = (EditText) findViewById(R.id.editDate);
+        EditText place = (EditText) findViewById(R.id.editLieux);
+        EditText result = (EditText) findViewById(R.id.editScore);
+
+        switch (view.getId()) {
+            case (R.id.addButton):
+                if (!nom.getText().toString().isEmpty() && !date.getText().toString().isEmpty() && !place.getText().toString().isEmpty() && !result.getText().toString().isEmpty() ) {
+Competition competition = new Competition();
+                    competition.setName(nom.getText().toString());
+                    competition.setLocation(place.getText().toString());
+                    competition.setScore(result.getText().toString());
+                    competition.setDate(date.getText().toString());
+
+
+                    DbUtil dbAccess = new DbUtil(this);
+
+                }
+        }
+
     }
 }
